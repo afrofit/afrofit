6d3c51b5a977cd4e83504e61035eee6c55f8d5bb
paddyodebiyi <paddy.o.dev@gmail.com>
2022-11-02 21:03:24 +0000

    Added in mixcloud

--- app/constants/device.ts
+++ app/constants/device.ts
@@ -4,8 +4,7 @@ export const DEVICE_WIDTH = Dimensions.get("window").width;
 
 export const DEVICE_HEIGHT = Dimensions.get("window").height;
 
-export const MIXCLOUD_URL =
-  "https://www.mixcloud.com/philipferrari/30-minutes-with-philip-ferrari-vol-44-clean/";
+export const MIXCLOUD_URL = "https://www.mixcloud.com/AFROFITAPP/";
 
 export const TEMP_VIDEO_LINK =
   "https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4";
